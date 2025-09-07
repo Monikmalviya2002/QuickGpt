252164a4a4c1f85bdd986ab1126b6af60fe7feef
Monik Malviya <monikmalviya04gmail.com>
2025-09-07 18:23:40 +0530

    created login and logout apis

--- routes/auth.js
+++ routes/auth.js
@@ -25,17 +25,46 @@ import jwt from "jsonwebtoken";
                      });
     
                         const saveUser = await user.save();
-                       const token = await jwt.sign({ _id: user._id}, "Monik@2002")
-
-                    res.cookie("token", token);
-                          res.json({ message: "User Added Successfully", data: saveUser });
+                      
+                        res.json({ message: "User Added Successfully", data: saveUser });
                     }
-        
-        
-            catch(err){
+                catch(err){
                 res.status(400).send("ERROR: " + err.message);
-            }     
+                }     
 
-   } )
+                } );
+
+         authRouter.post("/login" , async(req,res)=>{
+                try{
+                 const {emailId, password} = req.body;
+                 const user = await User.findOne({emailId : emailId})
+                if(!user){
+                throw new Error("invalide emailId and password");
+                }
+                 const isPasswordValid =  await bcrypt.compare(password, user.password)
+
+               if(isPasswordValid){
+          
+             const token = await jwt.sign({_id: user._id}, "Monik@2002")
+       
+
+             res.cookie("token", token);
+                  res.status(200).send({ message: "User Login successfully",user });
+               }
+               else{
+          throw new Error("invalide emailId and password")
+             }
+           } catch(err){
+           res.status(400).send("ERROR: " + err.message);
+           } 
+         });
+
+              authRouter.post("/logout" , async(req,res)=>{
+                    res.cookie("token", null,{
+                   expires: new Date(Date.now()),
+                   });   
+              res.send("log out successfully");
+                    })
+
 
   export default authRouter;
